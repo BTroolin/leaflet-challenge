73ea585727770d89ca74aeeb0095535f1233aff9
BTroolin <61754208+BTroolin@users.noreply.github.com>
2021-02-21 12:25:11 -0600

    updates to logic.js
    code for markers and plate boundaries

--- logic.js
+++ logic.js
@@ -1,31 +1,66 @@
 // url for weekly usgs earthquake data
-var QuakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+//var QuakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var quakes = "data/quakes.json";
 // url for tectonic plates borders
-var PlateURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+//var PlateURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+var plates = "data/PB2002_plates.json";
+var mag_radius = mag => mag * 20000;
 
-var myMap = L.map("map", {
-    center: [37.7749, -122.4194],
-    zoom: 5
+var mag_color = magnitude => {
+    var a = 1.0;
+    if (magnitude < 1) {
+        return `rgba(200,255,0,${a})`
+    } else if (magnitude < 2) {
+        return `rgba(211,204,0,${a})`
+    } else if (magnitude < 3) {
+        return `rgba(222,153,0,${a})`
+    } else if (magnitude < 4) {
+        return `rgba(233,102,0,${a})`
+    } else if (magnitude < 5) {
+        return `rgba(244,51,0,${a})`
+    } else {
+        return `rgba(255,0,0,${a})`
+    }
+}
+
+d3.json(quakes, data => {
+  data.json(plates, plate_data =>{
+    var layers = {
+      earthquakes: getEarthquakes(data),
+      faultlines: getFaultlines(plate_data)
+    };
+    createMap(layers);
   });
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: "pk.eyJ1IjoiYnRyb29saW4iLCJhIjoiY2s5MjZ2OW55MDVldDNobDVsN3Jybmk4YyJ9.bAKA3BfCyOkpSZZYA24KYw"
-  }).addTo(myMap);
+});
 
+var get_faults = plate_data => {
+    var plate_lines = L.geoJSON(plate_data, {
+        color: "yellow",
+        fillOpacity: 0
+    })
+    return plate_lines;
+}
 
+var get_quakes = quake_data => {
+    var cmarkers = [];
+    quake_data.features.forEach(feature => {
+        var crd = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+        var one_marker = L.circle(crd, {
+            stroke: false,
+            color : mag_color(feature.properties.mag),
+            fillColor: mag_color(feature.properties.mag),
+            fillOpacity: 1,
+            radius: mag_colors(feature.properties.mag)
+        });
 
+        one_marker.bindPopup(
+            "<h3>" + feature.properties.place + "<br>Magnitude: " + feature.properties.mag + "<br>Time: " + new Date(feature.properties.time)
+        );
+        cmarkers.push(one_marker);
+    });
 
-
-
-
-
-
-
+    return L.layerGroup(cmarkers);
+}
 
 
 
